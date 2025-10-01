a1d9fcb7150ae7dff7ceb907b47d3fe48eb5b285
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-10-01 08:22:52 +0000

    Define config.json

--- nixos-module.nix
+++ nixos-module.nix
@@ -1,21 +1,48 @@
 { pkgs, config, lib, ... }:
 let cfg = config.services.pifi; in {
-  options = {
-    services.pifi = {
-      enable = lib.mkEnableOption "pifi";
-      package = lib.mkOption {
-        description = "pifi package to use";
-        type = lib.types.package;
-      };
+  options.services.pifi = {
+    enable = lib.mkEnableOption "pifi";
+    package = lib.mkOption {
+      description = "pifi package to use";
+      type = lib.types.package;
+    };
+    port = lib.mkOption {
+      description = "Port to run on";
+      type = lib.types.int;
+      default = 3000;
+    };
+    mpd_host = lib.mkOption {
+      description = "MPD host";
+      type = lib.types.str;
+      default = "127.0.0.1";
+    };
+    mpd_port = lib.mkOption {
+      description = "MPD port";
+      type = lib.types.int;
+      default = 6600;
+    };
+    mpd_pass = lib.mkOption {
+      description = "MPD password";
+      type = lib.types.str;
+      default = "";
     };
   };
   config = lib.mkIf cfg.enable {
     systemd.services.pifi = {
       description = "Run the pifi web interface";
       serviceConfig = {
-        ExecStart = "${cfg.package}/bin/pifi";
+        ExecStart = "${cfg.package}/bin/pifi --port ${builtins.toString cfg.port}";
       };
       wantedBy = [ "multi-user.target" ];
     };
+    environment.etc."pifi/config.json".text = (builtins.toJSON {
+        mpd_host = cfg.mpd_host;
+        mpd_port = cfg.mpd_port;
+        mpd_password = cfg.mpd_pass;
+        streams_path_priv = "";
+        special_ips = [ ];
+        play_local = false;
+        serve_static = true;
+    });
   };
 }
