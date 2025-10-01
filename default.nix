01c71c98b909686c36ef7e07a68c0532bfc745e1
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-10-01 12:18:21 +0000

    Pin ruby 3.1

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ lib, bundlerApp, defaultGemConfig }:
+{ lib, bundlerApp, defaultGemConfig, ruby_3_1 }:
 bundlerApp {
   pname = "pifi";
   gemdir = ./.;
@@ -9,4 +9,8 @@ bundlerApp {
     };
   };
   exes = [ "pifi" ];
+  # ruby-mpd-0.3.3/lib/ruby-mpd.rb:273:in `socket': undefined method `exists?' for class File (NoMethodError)
+  # `exists?` was removed in ruby 3.2:
+  # https://stackoverflow.com/questions/14351272/undefined-method-exists-for-fileclass-nomethoderror
+  ruby = ruby_3_1;
 }
