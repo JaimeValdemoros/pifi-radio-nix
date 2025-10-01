fab692925a5fadefba6fa772a2ca6f6d512a8933
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-10-01 11:06:05 +0000

    Patch pifi to read CONFIG_PATH variable

--- default.nix
+++ default.nix
@@ -1,6 +1,12 @@
-{ bundlerApp }:
+{ lib, bundlerApp, defaultGemConfig }:
 bundlerApp {
   pname = "pifi";
   gemdir = ./.;
+  gemConfig = defaultGemConfig // {
+    pifi = attrs: {
+      dontBuild = false;
+      patches = [ ./config_getter.rb.patch ];
+    };
+  };
   exes = [ "pifi" ];
 }
