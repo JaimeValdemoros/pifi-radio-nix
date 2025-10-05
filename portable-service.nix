3d0bc7aff86a4063c10aa48484400326c16e8556
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-10-05 15:53:00 +0000

    Add pifi service file

--- portable-service.nix
+++ portable-service.nix
@@ -1,9 +1,18 @@
 {
+  concatText,
   portableService,
   pifi,
 }:
 portableService {
   pname = "pifi-radio";
   inherit (pifi) version;
-  units = [];
+  units = [
+    (concatText "pifi-radio.service" [./systemd/pifi-radio.service])
+  ];
+  symlinks = [
+    {
+      object = "${pifi}/bin/pifi";
+      symlink = "/bin/pifi";
+    }
+  ];
 }
